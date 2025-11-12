ac56b4cd236c2aa59a15126e69f9e105298ca4f9
harrisonchuckl <harrison@chuckl.co.uk>
2025-11-12 11:56:41 +0000

    Update start.ts

--- backend/src/start.ts
+++ backend/src/start.ts
@@ -1,10 +1,6 @@
-import createServer from "./server.js";
+import app from "./server.js";
 
 const port = Number(process.env.PORT ?? 8080);
-
-(async () => {
-  const app = await Promise.resolve(createServer());
-  app.listen(port, () => {
-    console.log(`Backend listening on :${port}`);
-  });
-})();
+app.listen(port, () => {
+  console.log(`Backend listening on :${port}`);
+});
